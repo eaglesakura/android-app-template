93b8b75f56fb13e122b4208ec2c31bdd9fba6dda
eaglesakura <eagle.sakura@gmail.com>
2016-12-19 17:06:04 +0900

    データ生成サンプルPropsを追加

---
+++ app/src/main/gen/java/replace/your/app_package/gen/prop/ExampleProps.java
@@ -0,0 +1,31 @@
+package replace.your.app_package.gen.prop;
+
+
+public class ExampleProps extends com.eaglesakura.android.property.internal.GeneratedProperties {
+    
+    public static final String ID_BOOLVALUE = "ExampleProps.boolValue";
+    public static final String ID_INTVALUE = "ExampleProps.intValue";
+    public static final String ID_LONGVALUE = "ExampleProps.longValue";
+    public static final String ID_FLOATVALUE = "ExampleProps.floatValue";
+    public static final String ID_DOUBLEVALUE = "ExampleProps.doubleValue";
+    public static final String ID_STRINGVALUE = "ExampleProps.stringValue";
+    public static final String ID_BYTEARRAYVALUE = "ExampleProps.byteArrayValue";
+    
+    public ExampleProps(){ }
+    public ExampleProps(com.eaglesakura.android.property.PropertyStore store) { setPropertyStore(store); }
+    public void setBoolValue(boolean set){ setProperty("ExampleProps.boolValue", set); }
+    public boolean isBoolValue(){ return getBooleanProperty("ExampleProps.boolValue"); }
+    public void setIntValue(int set){ setProperty("ExampleProps.intValue", set); }
+    public int getIntValue(){ return getIntProperty("ExampleProps.intValue"); }
+    public void setLongValue(long set){ setProperty("ExampleProps.longValue", set); }
+    public long getLongValue(){ return getLongProperty("ExampleProps.longValue"); }
+    public void setFloatValue(float set){ setProperty("ExampleProps.floatValue", set); }
+    public float getFloatValue(){ return getFloatProperty("ExampleProps.floatValue"); }
+    public void setDoubleValue(double set){ setProperty("ExampleProps.doubleValue", set); }
+    public double getDoubleValue(){ return getDoubleProperty("ExampleProps.doubleValue"); }
+    public void setStringValue(String set){ setProperty("ExampleProps.stringValue", set); }
+    public String getStringValue(){ return getStringProperty("ExampleProps.stringValue"); }
+    public void setByteArrayValue(byte[] set){ setProperty("ExampleProps.byteArrayValue", set); }
+    public byte[] getByteArrayValue(){ return getByteArrayProperty("ExampleProps.byteArrayValue"); }
+    
+}
