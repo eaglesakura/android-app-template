dbe8a8f9cdabe19b36b434cfd2e326a286b2197e
eaglesakura <eagle.sakura@gmail.com>
2017-01-20 19:04:37 +0900

    Web関連サンプルを追加

---
+++ app/src/main/java/replace/your/app_package/ui/widget/web/AppWebView.java
@@ -0,0 +1,17 @@
+package replace.your.app_package.ui.widget.web;
+
+import com.eaglesakura.material.widget.SupportWebView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * ユーザー表示用のコンテンツを指定するWebView
+ */
+public class AppWebView extends SupportWebView {
+
+    public AppWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
