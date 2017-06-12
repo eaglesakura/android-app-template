d6fe57787228df06cafe37a3029b6570a97c182a
eaglesakura <eagle.sakura@gmail.com>
2017-06-12 09:46:38 +0900

    非同期処理のExampleをLiveDataに変更 [ci skip] refs #4

--- app/src/main/java/replace/your/app_package/ui/navigation/example/ExampleFragmentMain.java
+++ app/src/main/java/replace/your/app_package/ui/navigation/example/ExampleFragmentMain.java
@@ -4,13 +4,15 @@ import com.eaglesakura.android.garnet.Inject;
 import com.eaglesakura.android.margarine.Bind;
 import com.eaglesakura.android.margarine.OnCheckedChanged;
 import com.eaglesakura.android.margarine.OnClick;
-import com.eaglesakura.cerberus.CallbackTime;
-import com.eaglesakura.cerberus.ExecuteTarget;
 import com.eaglesakura.sloth.annotation.FragmentLayout;
+import com.eaglesakura.sloth.app.lifecycle.Lifecycle;
+import com.eaglesakura.sloth.app.lifecycle.SlothLiveData;
 import com.eaglesakura.sloth.ui.license.LicenseViewActivity;
-import com.eaglesakura.sloth.ui.progress.ProgressToken;
+import com.eaglesakura.util.Timer;
 
 import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
@@ -41,23 +43,22 @@ public class ExampleFragmentMain extends AppNavigationFragment {
     @Bind(R.id.Example_Item_Debuggable)
     SwitchCompat mDebugSwitch;
 
+    @NonNull
+    AsyncTimeData mAsyncTimeData;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 非同期処理を行なう
-        getLifecycle().async(ExecuteTarget.LocalQueue, CallbackTime.FireAndForget, task -> {
-            try (ProgressToken token = pushProgress(R.string.EsMaterial_Word_Common_DataLoad)) {
-                // 適当な非同期処理
-                // ダミーで5秒間スリープ
-                task.waitTime(1000 * 5);
-                return this;
-            }
-        }).completed((result, task) -> {
-            AppLog.system("Hello World!!");
-        }).cancelSignal(this)
-                .canceled(task -> {
-                    AppLog.system("Task Canceled...");
-                }).start();
+        // 非同期テスト
+        mAsyncTimeData = new AsyncTimeData(getLifecycle());
+
+        mAsyncTimeData.observeAlive(getLifecycle(), time -> {
+            AppLog.test("observeAlive[%d]", time);
+        });
+
+        mAsyncTimeData.observeForeground(getLifecycle(), time -> {
+            AppLog.test("observeForeground[%d]", time);
+        });
     }
 
     @Nullable
@@ -84,5 +85,39 @@ public class ExampleFragmentMain extends AppNavigationFragment {
 
     public interface BindItem {
         String getTitle();
+    }
+
+    public static class AsyncTimeData extends SlothLiveData<Integer> {
+        Lifecycle mLifecycle;
+
+        CancellationSignal mTaskSignal;
+
+        public AsyncTimeData(Lifecycle lifecycle) {
+            mLifecycle = lifecycle;
+        }
+
+        @Override
+        protected void onActive() {
+            super.onActive();
+            CancellationSignal signal = new CancellationSignal();
+            mTaskSignal = signal;
+            mLifecycle.asyncQueue(task -> {
+                Timer timer = new Timer();
+                for (int i = 0; i < 10; ++i) {
+                    task.waitTime(1000);
+                    postValue((int) timer.end());
+                }
+                return this;
+            }).cancelSignal(task -> {
+                return signal.isCanceled();
+            }).start();
+        }
+
+        @Override
+        protected void onInactive() {
+            super.onInactive();
+            mTaskSignal.cancel();
+            mTaskSignal = null;
+        }
     }
 }
